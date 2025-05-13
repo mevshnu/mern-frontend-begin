689d08d4ec64477bc6dc93746a078e7eca97b883
Vshnu <vshnu@Vshnus-Laptop.local>
2025-05-13 18:34:36 +0200

    addind components and pages and edit in app and main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <button>click</button>
+    <Routes>
+      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/login' element={<h1>Login</h1>} />
+      <Route path='/register' element={<h1>Register</h1>} />
+    </Routes>
     </>
   )
 }
